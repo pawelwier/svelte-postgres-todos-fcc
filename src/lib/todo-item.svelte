57fdde6761b1cbbeea03a32a7672934e5b10c7c3
pawelwier <pawiercinski@gmail.com>
2022-05-23 21:46:41 +0200

    css styles 1

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,4 +1,71 @@
-<div>
+<style>
+  .todo {
+    display: grid;
+    grid-template-columns: 2rem 1fr 2rem;
+    grid-gap: 0.5rem;
+    align-items: center;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem;
+    border-radius: 8px;
+    background-color: #FFF;
+    filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.2));
+    transform: translate(-1px, -1px);
+    transition: filter 0.2s, transform 0.2s;
+  }
+
+  .todo button {
+    width: 2rem;
+    height: 2rem;
+    border: none;
+    background-color: transparent;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+  }
+
+  .todo input {
+    flex: 1;
+    padding: 0.5rem 2rem 0.5rem 0.8rem;
+    border-radius: 3px;
+  }
+
+  .toggle {
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-size: 1rem auto;
+  }
+
+  .text {
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex: 1;
+  }
+
+  .save {
+    position: absolute;
+    right: 0;
+    opacity: 0;
+  }
+
+  .todo input:focus + .save,
+  .save:focus {
+    transition: opacity 0.2s;
+    opacity: 1;
+  }
+
+  .delete {
+    opacity: 0.3;
+  }
+
+  .delete:hover,
+  .delete:focus {
+    transition: 0.2s opacity;
+    opacity: 1;
+  }
+</style>
+
+<div class="todo">
   <form 
     action=""
     method=""
@@ -9,12 +76,14 @@
       value=""
     />
     <button
+      class="toggle"
       aria-label="Mark done/not done"
     >
-      Done/Not done
+      Done
     </button>
   </form>
   <form 
+    class="text"
     action=""
     method=""
   >
@@ -24,6 +93,7 @@
       value=""
     />
     <button
+      class="save"
       aria-label="Save changes"
     >
       Save
@@ -34,6 +104,7 @@
     method=""
   >
     <button
+      class="delete"
       aria-label="Delete"
     >
       Delete
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,21 +4,59 @@
   const title = 'Todos'
 </script>
 
+<style>
+  .todos {
+    width: 100%;
+    max-width: 42rem;
+    margin: 4rem auto 0 auto;
+  }
+
+  .new {
+    margin-bottom: 0.6rem;
+  }
+
+  .new input {
+    font-size: 1.2rem;
+    width: 100%;
+    padding: 0.5rem 1rem 0.2rem 1rem;
+    background: rgba(255, 255, 255, 0.05);
+    box-sizing: border-box;
+    border-radius: 8px;
+    text-align: center;
+  }
+
+  .todos :global(input) {
+    border: none;
+  }
+
+  .todos :global(input:focus-visible) {
+    box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.2);
+    border: 1px solid rgb(66, 35, 150);
+    outline: none;
+  }
+</style>
+
 <svelte:head>
   <title>
     {title}
   </title>
 </svelte:head>
 
-<h1>{title}</h1>
-
-<form action="" method="">
-  <input 
-    type="text" 
-    name="new-todo" 
-    aria-label="Add a todo" 
-    placeholder="+ tap to add a todo"
+<div class="todos">
+  <h1>{title}</h1>
+  <form
+    action="" 
+    method=""
+    class="new"
   >
-</form>
+    <input
+      type="text" 
+      name="new-todo" 
+      aria-label="Add a todo" 
+      placeholder="+ tap to add a todo"
+    >
+  </form>
+  <TodoItem />
+  <TodoItem />
 
-<TodoItem />
+</div>
